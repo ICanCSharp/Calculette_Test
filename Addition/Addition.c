ad3250176d25fe3a6189a8a8c2a0e2b52f15ab81
themanwithagreenhood <themanwithagreenhood@gmail.com>
2017-12-06 11:10:50 -0500

    2e commit

---
+++ Addition/Addition.c
@@ -0,0 +1,18 @@
+// Bon, on dit que c'est un fichier en C
+
+int main ()
+{
+
+int a = 0;
+int b = 0;
+
+int resultat = 0;
+
+scanf("%d",&a);
+scanf("%d",&b);
+
+resultat = a + b;
+
+printf("Le résultat est %d", resultat);
+return 0;
+}
